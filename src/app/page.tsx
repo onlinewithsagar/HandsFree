783fda5a236c74f9c8b00a0e0811a211902cd9b3
Sreeju7733 <moundadanpdn@gmail.com>
2026-08-29 18:58:37 +0530

    feat: add LogoConvergence component to homepage and include new hand assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,6 +75,7 @@ export default function Home() {
         <WorkflowStudio />
         <StatsCalculator />
         <About />
+        <LogoConvergence />
         <Testimonials />
         <Contact onPlaySound={playSound} />
         <Footer />
